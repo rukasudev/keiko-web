191d25bbc29439bca68bd71f2d7694cc3e6d7231
Lucas Daniel <lucasdanielbarradas@gmail.com>
2024-10-13 17:41:39 -0300

    chore: fixing i18nProvider eslint and adding .eslintignore

--- providers/i18nProvider.tsx
+++ providers/i18nProvider.tsx
@@ -1,25 +1,32 @@
-"use client"
+'use client'
 
 import { createContext, ReactNode } from 'react'
 import { Dictionary } from '@/dictionaries'
 
 interface I18nProviderProps {
-  lang: string;
-  dictionary: Dictionary;
-  children: ReactNode;
+  lang: string
+  dictionary: Dictionary
+  children: ReactNode
 }
 
 type I18nContextType = {
-  i18n: Dictionary;
-  lang: string;
+  i18n: Dictionary
+  lang: string
 }
 
-export const I18nContext = createContext<I18nContextType>({ i18n: {}, lang: '' });
+export const I18nContext = createContext<I18nContextType>({
+  i18n: {},
+  lang: '',
+})
 
-export const I18nProvider: React.FC<I18nProviderProps> = ({ dictionary, lang, children }) => {
+export const I18nProvider: React.FC<I18nProviderProps> = ({
+  dictionary,
+  lang,
+  children,
+}) => {
   return (
-    <I18nContext.Provider value={{ i18n: dictionary, lang: lang }}>
+    <I18nContext.Provider value={{ i18n: dictionary, lang }}>
       {children}
     </I18nContext.Provider>
-  );
+  )
 }
